3cd7782c798ed9d40f33c4e4e04ad656f6ed8363
PavelGith <linevich.p.l@gmail.com>
2023-04-27 15:29:48 +0300

    refactor: change fromCharCode to arr string

--- scripts/body-builder.js
+++ scripts/body-builder.js
@@ -1,12 +1,11 @@
-const keyboardBottuns = [96, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 45, 61, 8, 9, 113, 119,
-  101, 114, 116, 121, 117, 105, 111, 112, 91, 93, 92, 46, 0, 97, 115, 100, 102, 103,
-  104, 106, 107, 108, 59, 39, 13, 16, 122, 120, 99, 118, 98, 110, 109, 44, 46, 47, 38,
-  16, 17, 1000, 18, 32, 18, 37, 40, 39, 17];
+const keyboardButtonsEn = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\',
+  'CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', 'Enter', 'Ctrl', 'Alt', 'Space', 'Alt', 'Ctrl'
+];
 
 function initKeyboard() {
   let out = '';
-  for (let i = 0; i < keyboardBottuns.length; i += 1) {
-    out += `<div class="key">${String.fromCharCode(keyboardBottuns[i])}</div>`;
+  for (let i = 0; i < keyboardButtonsEn.length; i += 1) {
+    out += `<div class="key">${keyboardButtonsEn[i]}</div>`;
   }
   document.querySelector('.keyboard').innerHTML = out;
 }
@@ -30,6 +29,7 @@ function indexBuild() {
   const keyboard = document.createElement('div');
   main.append(keyboard);
   keyboard.classList.add('keyboard');
+  keyboard.setAttribute('id', 'keyboard');
 
   const description = document.createElement('p');
   main.append(description);
