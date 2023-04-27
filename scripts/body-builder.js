d62580d040862d9f650aa5d70325dbd3ec0d8982
PavelGith <linevich.p.l@gmail.com>
2023-04-27 13:47:51 +0300

    feat: add function generation keyboard

--- scripts/body-builder.js
+++ scripts/body-builder.js
@@ -1,4 +1,15 @@
-const keyboard = [];
+const keyboard = [96, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 45, 61, 8, 9, 113, 119,
+  101, 114, 116, 121, 117, 105, 111, 112, 91, 93, 92, 46, 0, 97, 115, 100, 102, 103,
+  104, 106, 107, 108, 59, 39, 13, 16, 122, 120, 99, 118, 98, 110, 109, 44, 46, 47, 38,
+  16, 17, 1000, 18, 32, 18, 37, 40, 39, 17];
+
+function initKeyboard() {
+  let out = '';
+  for (let i = 0; i < keyboard.length; i++) {
+    out += '<div class="key">' + String.fromCharCode(keyboard[i]) + '</div>';
+  }
+  document.querySelector('.keyboard').innerHTML = out;
+}
 
 function indexBuild() {
   let body = document.querySelector('body');
@@ -29,6 +40,8 @@ function indexBuild() {
   main.append(language);
   language.classList.add('language');
   language.innerText = `To switch the language, the combination is: shift + alt.`;
+
+  initKeyboard();
 }
 
 indexBuild();
