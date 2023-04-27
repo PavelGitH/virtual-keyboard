be1d23e6e87b004fb78b5bad504d75cc7a2949f4
PavelGith <linevich.p.l@gmail.com>
2023-04-27 18:12:22 +0300

    feat: keyboard rendering completed

--- scripts/body-builder.js
+++ scripts/body-builder.js
@@ -1,13 +1,72 @@
-const keyboardButtonsEn = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\',
-  'CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', 'Enter', 'Ctrl', 'Alt', 'Space', 'Alt', 'Ctrl'
-];
+const keyboardButtonsEng = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', 'Enter', 'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '↑', 'Shift', 'Ctrl', 'Win', 'Alt', 'Space', 'Alt', '←', '↓', '→', 'Ctrl'];
+
+const keyboardButtonsRus = ['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\\', 'CapsLock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter', 'Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', '↑', 'Shift', 'Ctrl', 'Win', 'Alt', 'Space', 'Alt', '←', '↓', '→', 'Ctrl'];
+
+let lang = [];
+
+if (localStorage.language === 'rus') {
+  lang = keyboardButtonsRus;
+} else {
+  lang = keyboardButtonsEng;
+}
 
 function initKeyboard() {
-  let out = '';
-  for (let i = 0; i < keyboardButtonsEn.length; i += 1) {
-    out += `<div class="key">${keyboardButtonsEn[i]}</div>`;
-  }
-  document.querySelector('.keyboard').innerHTML = out;
+  const fragment = document.createDocumentFragment();
+
+  lang.forEach((elem) => {
+    const button = document.createElement('div');
+    button.classList.add('key');
+
+    switch (elem) {
+      case 'Backspace':
+        button.classList.add('big-key', 'backspace');
+        button.innerHTML = 'Backspace';
+        break;
+
+      case 'Enter':
+        button.classList.add('big-key', 'enter');
+        button.innerHTML = 'Enter';
+        break;
+
+      case 'Shift':
+        button.classList.add('big-key', 'shift');
+        button.innerHTML = 'Shift';
+        break;
+
+      case 'Tab':
+        button.classList.add('big-key', 'tab');
+        button.innerHTML = 'Tab';
+        break;
+
+      case 'CapsLock':
+        button.classList.add('big-key', 'caps');
+        button.innerHTML = 'CapsLock';
+        break;
+
+      case 'Space':
+        button.classList.add('space-key');
+        button.innerText = ' ';
+        break;
+
+      case 'Alt':
+        button.classList.add('alt');
+        button.innerHTML = 'Alt';
+        break;
+
+      case 'Ctrl':
+        button.classList.add('ctrl');
+        button.innerHTML = 'Ctrl';
+        break;
+
+      default:
+        button.innerText = elem;
+        break;
+    }
+
+    fragment.append(button);
+  });
+
+  return fragment;
 }
 
 function indexBuild() {
@@ -30,6 +89,7 @@ function indexBuild() {
   main.append(keyboard);
   keyboard.classList.add('keyboard');
   keyboard.setAttribute('id', 'keyboard');
+  keyboard.append(initKeyboard());
 
   const description = document.createElement('p');
   main.append(description);
@@ -41,7 +101,8 @@ function indexBuild() {
   language.classList.add('language');
   language.innerText = 'To switch the language, the combination is: shift + alt.';
 
-  initKeyboard();
+  // как взять язык у клиента?
+  localStorage.setItem('language', 'eng');
 }
 
 indexBuild();
