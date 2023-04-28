4de6a68399838bb3f6160655930e35ea390b1832
PavelGith <linevich.p.l@gmail.com>
2023-04-28 15:28:53 +0300

    feat: add mouse input function

--- scripts/body-builder.js
+++ scripts/body-builder.js
@@ -1,13 +1,13 @@
 const keyboardButtonsEng = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', 'Enter', 'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '↑', 'Shift', 'Ctrl', 'Win', 'Alt', 'Space', 'Alt', '←', '↓', '→', 'Ctrl'];
-
 const keyboardButtonsRus = ['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\\', 'CapsLock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter', 'Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', '↑', 'Shift', 'Ctrl', 'Win', 'Alt', 'Space', 'Alt', '←', '↓', '→', 'Ctrl'];
-
+const text = [];
 let lang = [];
 
 if (localStorage.language === 'rus') {
   lang = keyboardButtonsRus;
 } else {
   lang = keyboardButtonsEng;
+  localStorage.setItem('language', 'eng');
 }
 
 function initKeyboard() {
@@ -18,48 +18,42 @@ function initKeyboard() {
     button.classList.add('key');
 
     switch (elem) {
-      case 'Backspace':
-        button.classList.add('big-key', 'backspace');
-        button.innerHTML = 'Backspace';
-        break;
-
-      case 'Enter':
-        button.classList.add('big-key', 'enter');
-        button.innerHTML = 'Enter';
-        break;
-
-      case 'Shift':
-        button.classList.add('big-key', 'shift');
-        button.innerHTML = 'Shift';
-        break;
-
-      case 'Tab':
-        button.classList.add('big-key', 'tab');
-        button.innerHTML = 'Tab';
-        break;
-
-      case 'CapsLock':
-        button.classList.add('big-key', 'caps');
-        button.innerHTML = 'CapsLock';
-        break;
-
-      case 'Space':
-        button.classList.add('space-key');
-        button.innerText = ' ';
-        break;
-
       case 'Alt':
         button.classList.add('alt');
         button.innerHTML = 'Alt';
         break;
-
+      case 'Backspace':
+        button.classList.add('backspace');
+        button.innerHTML = 'Backspace';
+        break;
+      case 'CapsLock':
+        button.classList.add('caps');
+        button.innerHTML = 'CapsLock';
+        break;
       case 'Ctrl':
         button.classList.add('ctrl');
         button.innerHTML = 'Ctrl';
         break;
+      case 'Enter':
+        button.classList.add('enter');
+        button.innerHTML = 'Enter';
+        break;
+      case 'Shift':
+        button.classList.add('shift');
+        button.innerHTML = 'Shift';
+        break;
+      case 'Space':
+        button.classList.add('space');
+        button.innerText = ' ';
+        break;
+      case 'Tab':
+        button.classList.add('tab');
+        button.innerHTML = 'Tab';
+        break;
 
       default:
         button.innerText = elem;
+        button.classList.add('key-simple');
         break;
     }
 
@@ -96,13 +90,34 @@ function indexBuild() {
   description.classList.add('description');
   description.innerText = 'The keyboard was created in the Windows operating system.';
 
-  const language = document.createElement('p');
-  main.append(language);
-  language.classList.add('language');
-  language.innerText = 'To switch the language, the combination is: shift + alt.';
+  const languages = document.createElement('p');
+  main.append(languages);
+  languages.classList.add('languages');
+  languages.innerText = 'To switch the language, the combination is: shift + alt.';
+}
 
-  // how to take the language from the client?
-  localStorage.setItem('language', 'eng');
+function mouseInput() {
+  document.addEventListener('click', (event) => {
+    if (event.target.classList.contains('key-simple')) {
+      text.push(event.target.innerText);
+    }
+    if (event.target.classList.contains('space')) {
+      text.push(' ');
+    }
+    if (event.target.classList.contains('enter')) {
+      text.push('\n');
+      document.querySelector('textarea').innerHTML = ('br');
+    }
+    if (event.target.classList.contains('tab')) {
+      text.push('    ');
+    }
+    if (event.target.classList.contains('backspace')) {
+      text.splice(text.length - 1, 1);
+    }
+    localStorage.text = text.join('');
+    document.querySelector('textarea').innerHTML = localStorage.text;
+  });
 }
 
 indexBuild();
+mouseInput();
